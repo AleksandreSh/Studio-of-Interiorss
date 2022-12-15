2bebf765db22fe6a120b3ed46ec4c9c81c7e1528
AleksandreSh <111062220+AleksandreSh@users.noreply.github.com>
2022-12-15 13:30:14 +0200

    ..

--- src/js/script.js
+++ src/js/script.js
@@ -1085,7 +1085,7 @@
     /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
     /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
     /******/ 				}
-    /******/ 			}
+    /******/ 			} 
     /******/ 		};
     /******/ 	})();
     /******/ 	
